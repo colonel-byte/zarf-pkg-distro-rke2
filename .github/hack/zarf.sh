b00ef4433e73e1f86fac0ea4058935b60bde1315
Allen Conlon <software@conlon.dev>
2025-07-30 18:12:35 -0400

    feat: update for how rke2 service are installed

--- .github/hack/zarf.sh
+++ .github/hack/zarf.sh
@@ -25,3 +25,17 @@ for arch in "${ARCH[@]}"; do
   echo "::debug::pulling air-gap image list"
   curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} -s -L -o files/images-core.linux-$arch.txt https://github.com/rancher/rke2/releases/download/$RKE2_VERSION/rke2-images-core.linux-$arch.txt
 done
+
+echo "::debug::pulling down the rke2 linux tar bundle"
+curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} -s -L -o ./.direnv/bin/rke2.linux-amd64.tar.gz https://github.com/rancher/rke2/releases/download/$RKE2_VERSION/rke2.linux-amd64.tar.gz
+
+echo "::debug::extracting the contents of the rke2 linux tar bundle"
+zarf tools archiver decompress .direnv/bin/rke2.linux-amd64.tar.gz .direnv/bin/
+
+echo "::debug::copy and update the agent service file to be compatible with zarf config"
+cp .direnv/bin/lib/systemd/system/rke2-agent.service  files/rke2-agent.service
+sed -i 's!/usr/local/bin/rke2 agent!/var/lib/rancher/rke2/bin/rke2 agent ###ZARF_VAR_RKE2_ARGS###!g' files/rke2-agent.service
+
+echo "::debug::copy and update the server service file to be compatible with zarf config"
+cp .direnv/bin/lib/systemd/system/rke2-server.service files/rke2-server.service
+sed -i 's!/usr/local/bin/rke2 server!/var/lib/rancher/rke2/bin/rke2 server ###ZARF_VAR_RKE2_ARGS###!g' files/rke2-server.service
